09de679a937b5ad314409e7bc6925fa383e919c1
SaturnX-Dev <SaturnX-Dev@outlook.com>
2025-12-24 00:42:38 -0600

    chore: release v0.1.31

--- beatscrobble-android/app/build.gradle.kts
+++ beatscrobble-android/app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "0.1.30"
+        versionName = "0.1.31"
 
         vectorDrawables {
             useSupportLibrary = true
